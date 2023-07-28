c1278a7a4811eed88f07746cef425b4171d31d9e
X-Tripen <hubertpaprocki.dev@gmail.com>
2023-07-28 13:01:21 +0200

    feat:add empty about page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+function AboutPage() {
+  return (
+    <>
+      <Head>
+        <title>About me</title>
+      </Head>
+      <div>AboutPage</div>
+    </>
+  );
+}
+
+export default AboutPage;
