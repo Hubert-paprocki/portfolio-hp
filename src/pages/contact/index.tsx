b1fcfa6e30238cfe3629a52095c52e4233fbd3c4
X-Tripen <hubertpaprocki.dev@gmail.com>
2023-07-28 13:01:43 +0200

    feat:add empty contact page

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+function ContactPage() {
+  return (
+    <>
+      <Head>
+        <title>Contact me</title>
+      </Head>
+      <div>ContactPage</div>
+    </>
+  );
+}
+
+export default ContactPage;
